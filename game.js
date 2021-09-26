f0acaab438a0c52ed0203cf21374b2a7ec46ebd8
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-26 15:37:52 -0600

    Display a win on the DOM

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ class Game {
   }
 
   startNewGame() {
+    console.log('startNewGame');
     //Once we have saveWinsToStorage, create a conditional that allows us to
     //retrieveWinsFromStorage and reinstantiate the players, else if saveWinsToStorage is null 
     //then we just want to create brand new player instances instead of using the old ones
@@ -21,15 +22,15 @@ class Game {
     this.players = [player1, player2];
   }
 
-  getOpenSquares() {
-    var openSquares = [];
+  getOpenSquareIndices() {
+    var openSquareIndices = [];
 
     for (var i = 0; i < this.gameBoard.length; i++) {
       if (!this.gameBoard[i]) {
-        openSquares.push(i);
+        openSquareIndices.push(i);
       }
     }
-    return openSquares;
+    return openSquareIndices;
   }
 
   selectSquare(indexOfSquare) {
@@ -45,7 +46,7 @@ class Game {
     var winByRow = this.checkForRows(); 
     var winByDiagonal = this.checkForDiagonals();
 
-    return winByColumn || winByRow || winByDiagonal;
+    return Boolean(winByColumn || winByRow || winByDiagonal);
   }
 
   checkForColumns() {
@@ -63,7 +64,7 @@ class Game {
     var column2Win = one === four && four === seven && one;
     var column3Win = two === five && five === eight && two;
 
-    return column1Win || column2Win || column3Win;
+    return Boolean(column1Win || column2Win || column3Win);
   }
 
   checkForRows() {
@@ -81,7 +82,7 @@ class Game {
     var row2Win = three === four && four === five && three;
     var row3Win = six === seven && seven === eight && six;
 
-    return row1Win || row2Win || row3Win;
+    return Boolean(row1Win || row2Win || row3Win);
   }
 
   checkForDiagonals () {
@@ -94,7 +95,7 @@ class Game {
     var diagonal1Win = zero === four && four === eight && zero;
     var diagonal2Win = six === four && four === two && six;
 
-    return diagonal1Win || diagonal2Win;
+    return Boolean(diagonal1Win || diagonal2Win);
   }
 
   incrementPlayerWins() {
--- main.js
+++ main.js
@@ -2,23 +2,33 @@ var game = new Game();
 game.startNewGame();
 
 var gameBoard = document.querySelector('.game-board');
-gameBoard.addEventListener('click', handleSquareClicks);
-function handleSquareClicks(event) {
-  var selectedSquareId = Number(event.target.closest('.align-player-icon').id);
-  var openSquareIndices = game.getOpenSquareIndices();
+var turnAndWinnerDisplay = document.querySelector('.turn-and-winner-display');
+var babyYodaNumberOfWinsDisplay = document.querySelector('.player-1-number-of-wins');
+var mandoNumberOfWinsDisplay = document.querySelector('.player-2-number-of-wins');
 
+gameBoard.addEventListener('click', handleSquareClicks);
+
+
+function handleSquareClicks(event) {
+  var selectedSquareId = Number(event.target.closest('.game-board-square').id);
+  var openSquareIndices = game.getOpenSquareIndices();
+  
   if (openSquareIndices.length !== 0) {
     for (var i = 0; i < openSquareIndices.length; i++) {
       if (selectedSquareId === openSquareIndices[i]) {
         game.selectSquare(selectedSquareId);
         displayTokens();
-        game.checkForWin();
-      }
-      if (game.checkForWin()) {
-        displayWinner();
-        game.incrementPlayerWins();
-        displayUpdatedPlayerWins();
-        //setTimeout(game.startNewGame(), 5000);
+        if (game.checkForWin()) {
+          gameBoard.classList.add('disable-click'); //doesn't allow players to select a square
+          displayWinner();
+          game.incrementPlayerWins();
+          //store player wins ~ should we just save the wins of the player who won or both?
+          //storing the wins for the player that won
+          game.players[game.activePlayerIndex].saveWinsToStorage();
+          displayUpdatedPlayerWins();
+          timeOut();
+          game.startNewGame();
+        }
       }
     }
   } else if (!openSquareIndices.length) {
@@ -32,7 +42,7 @@ function handleSquareClicks(event) {
 };
 
   function displayTokens() {
-    var squares = document.querySelectorAll('.align-player-icon');
+    var squares = document.querySelectorAll('.game-board-square');
     var alt;
     for (var i = 0; i < squares.length; i++) {
       if (game.gameBoard[i] !== null) {
@@ -47,3 +57,54 @@ function handleSquareClicks(event) {
       }
     }
   }
+
+  function displayWinner() {
+    var alt;
+    if (game.players[game.activePlayerIndex].token === "./assets/baby-yoda.jpg") {
+      alt = "baby yoda picture";
+    } else {
+      alt = "mando picture";
+    }
+    
+    turnAndWinnerDisplay.innerHTML = `
+      <img src="${game.players[game.activePlayerIndex].token}" alt="${alt}" class="winner-display player-image">
+      <p> won! </p>
+    `;
+  }
+
+  function displayUpdatedPlayerWins() {
+      babyYodaNumberOfWinsDisplay.innerText = `Number of Wins: ${game.players[0].wins}`;
+      mandoNumberOfWinsDisplay.innerText = `Number of Wins: ${game.players[1].wins}`;
+  }
+
+  function timeOut() {
+    setTimeout(clearTheGameBoard, 2000);
+  }
+
+  function clearTheGameBoard() {
+    var squares = document.querySelectorAll('.game-board-square');
+    for (var i = 0; i < game.gameBoard.length; i++) {
+      squares[i].innerHTML = ``;
+    }
+    gameBoard.classList.remove('disable-click');//allows players to select a square
+  }
+
+
+// if event square is in opensquares {
+//   selectSquare(eventSquare)
+//   if (checkForWin()) {
+//   //update DOM to say who won, update their total number of wins and display it, reset the game/start it over
+//     updateDomToSayWhoWon();
+//     incrementPlayerWins();
+//     updateDomToDisplayPlayerWins();
+//     waitACoupleOfSeconds();
+//     startNewGame();
+//   } else if (!getOpenSquares()) {
+//     //update the DOM to say it's a draw, reset the game/start it over
+//     updateDomToSayThereWasADraw();
+//     waitACoupleOfSeconds();
+//     startNewGame();
+//   } 
+//   switchActivePlayer();
+//   updateDomToSayWhosTurnItIs();
+// }
