ec509ada285e84e9c5eb6482351d217df4e095a8
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-27 10:28:39 -0600

    Add functionality for draws and turn indicators

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-var game = new Game();
-game.startNewGame();
-
+var game;
 var gameBoard = document.querySelector('.game-board');
 var turnAndWinnerDisplay = document.querySelector('.turn-and-winner-display');
 var babyYodaNumberOfWinsDisplay = document.querySelector('.player-1-number-of-wins');
 var mandoNumberOfWinsDisplay = document.querySelector('.player-2-number-of-wins');
 
 gameBoard.addEventListener('click', handleSquareClicks);
+window.addEventListener('load', loadGame);
 
+function loadGame() {
+  game = new Game();
+  game.startNewGame();
+
+}
 
 function handleSquareClicks(event) {
   var selectedSquareId = Number(event.target.closest('.game-board-square').id);
@@ -19,75 +23,95 @@ function handleSquareClicks(event) {
         game.selectSquare(selectedSquareId);
         displayTokens();
         if (game.checkForWin()) {
-          gameBoard.classList.add('disable-click'); //doesn't allow players to select a square
           displayWinner();
+          gameBoard.classList.add('disable-click');
           game.incrementPlayerWins();
-          //store player wins ~ should we just save the wins of the player who won or both?
-          //storing the wins for the player that won
-          game.players[game.activePlayerIndex].saveWinsToStorage();
+          game.players[game.activePlayerIndex].saveWinsToStorage(game.players);
           displayUpdatedPlayerWins();
           timeOut();
-          game.startNewGame();
+          return;
+        } else if (!game.getOpenSquareIndices().length) {
+          turnAndWinnerDisplay.innerHTML = `
+            <p>
+              It's a draw
+            </p>
+          `;
+          gameBoard.classList.add('disable-click');
+          timeOut();
+          return;
         }
+        game.switchActivePlayer();
+        displayNewActivePlayer();
       }
     }
-  } else if (!openSquareIndices.length) {
-    //update the DOM to say it's a draw, reset the game/start it over
-//     updateDomToSayThereWasADraw();
-//     waitACoupleOfSeconds();
-//     startNewGame();
-  }
-  game.switchActivePlayer();
-  //displayNewActivePlayer();
-};
+  } 
+}
 
-  function displayTokens() {
-    var squares = document.querySelectorAll('.game-board-square');
-    var alt;
-    for (var i = 0; i < squares.length; i++) {
-      if (game.gameBoard[i] !== null) {
-        if (game.gameBoard[i] === "./assets/baby-yoda.jpg") {
-          alt = "baby yoda picture";
-        } else {
-          alt = "mando picture";
-        }
-        squares[i].innerHTML = `
-          <img src="${game.gameBoard[i]}" alt="${alt}" class="player-image">
-        `;
-      }
-    }
-  }
-
-  function displayWinner() {
-    var alt;
-    if (game.players[game.activePlayerIndex].token === "./assets/baby-yoda.jpg") {
+function displayTokens() {
+  var squares = document.querySelectorAll('.game-board-square');
+  var alt;
+  for (var i = 0; i < squares.length; i++) {
+    if (game.gameBoard[i] === "./assets/baby-yoda.jpg") {
       alt = "baby yoda picture";
     } else {
       alt = "mando picture";
     }
-    
-    turnAndWinnerDisplay.innerHTML = `
-      <img src="${game.players[game.activePlayerIndex].token}" alt="${alt}" class="winner-display player-image">
-      <p> won! </p>
-    `;
-  }
-
-  function displayUpdatedPlayerWins() {
-      babyYodaNumberOfWinsDisplay.innerText = `Number of Wins: ${game.players[0].wins}`;
-      mandoNumberOfWinsDisplay.innerText = `Number of Wins: ${game.players[1].wins}`;
-  }
-
-  function timeOut() {
-    setTimeout(clearTheGameBoard, 2000);
-  }
-
-  function clearTheGameBoard() {
-    var squares = document.querySelectorAll('.game-board-square');
-    for (var i = 0; i < game.gameBoard.length; i++) {
+    if (game.gameBoard[i] !== null) {
+      squares[i].innerHTML = `
+        <img src="${game.gameBoard[i]}" alt="${alt}" class="player-image">
+      `;
+    } else {
       squares[i].innerHTML = ``;
     }
-    gameBoard.classList.remove('disable-click');//allows players to select a square
+  } 
+}
+
+function displayWinner() {
+  var alt;
+  if (game.players[game.activePlayerIndex].token === "./assets/baby-yoda.jpg") {
+    alt = "baby yoda picture";
+  } else {
+    alt = "mando picture";
   }
+  
+  turnAndWinnerDisplay.innerHTML = `
+    <img src="${game.players[game.activePlayerIndex].token}" alt="${alt}" class="winner-display player-image">
+    <p> won! </p>
+  `;
+}
+
+function displayNewActivePlayer() {
+  var alt;
+  if (game.players[game.activePlayerIndex].token === "./assets/baby-yoda.jpg") {
+    alt = "baby yoda picture";
+  } else {
+    alt = "mando picture";
+  }
+
+  turnAndWinnerDisplay.innerHTML = `
+    <p>It's </p>
+    <img src="${game.players[game.activePlayerIndex].token}" alt="${alt}" class="winner-display player-image">
+    <p>'s turn</p>
+  `;
+}
+
+function displayUpdatedPlayerWins() {
+  babyYodaNumberOfWinsDisplay.innerText = `Number of Wins: ${game.players[0].wins}`;
+  mandoNumberOfWinsDisplay.innerText = `Number of Wins: ${game.players[1].wins}`;
+}
+
+function timeOut() {
+  setTimeout(clearTheGameBoard, 2000);
+}
+
+function clearTheGameBoard() {
+  game.startNewGame();
+  displayTokens();
+  gameBoard.classList.remove('disable-click');
+  game.switchActivePlayer();
+  displayNewActivePlayer();
+}
+
 
 
 // if event square is in opensquares {
