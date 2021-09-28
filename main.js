ad32fe2b94dde1a998f21b8f13589573821128f0
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-28 09:56:43 -0600

    Remove the padding fnd rhandle square clicks outside of the gameboard

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ function loadGame() {
 }
 
 function handleSquareClicks(event) {
+  if (!event.target.closest('.game-board-square')) {
+    return;
+  }
   var selectedSquareId = Number(event.target.closest('.game-board-square').id);
   var openSquareIndices = game.getOpenSquareIndices();
   for (var i = 0; i < openSquareIndices.length; i++) {
